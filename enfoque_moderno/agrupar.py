dff928adc90eb9077e6ba2c8e2b8344009f89436
René Elizalde <reroes799@gmail.com>
2026-04-24 23:59:09 -0500

    archivos para explicar

--- enfoque_moderno/agrupar.py
+++ enfoque_moderno/agrupar.py
@@ -13,19 +13,21 @@ start = time.perf_counter()
 
 resultado = (
     pl.scan_csv(DATA)
-    .filter(
-        (pl.col("rol") == "estudiante") &
-        (pl.col("actividad") == "Tarea") &
-        (pl.col("estado_entrega") == "tardío")
-    )
+    .filter(pl.col("nota").is_not_null())
+    .group_by("curso")
+    .agg([
+        pl.col("nota").mean().alias("promedio_nota"),
+        pl.col("nota").count().alias("total_registros")
+    ])
     .collect()
 )
 
 end = time.perf_counter()
 end_mem = mem_mb()
 
-print("\n[ENFOQUE MODERNO]")
-# print(resultado.head())
+print("\n[ENFOQUE MODERNO - GROUPBY]")
+print(resultado)
+
 print(f"\nFilas: {resultado.height}")
 print(f"Tiempo: {end - start:.2f} s")
 print(f"Memoria usada: {end_mem - start_mem:.2f} MB")
--- enfoque_tradicional/agrupar.py
+++ enfoque_tradicional/agrupar.py
@@ -13,17 +13,22 @@ start = time.perf_counter()
 
 df = pd.read_csv(DATA)
 
-resultado = df[
-    (df["rol"] == "estudiante") &
-    (df["actividad"] == "Tarea") &
-    (df["estado_entrega"] == "tardío")
-]
+resultado = (
+    df[df["nota"].notna()]
+    .groupby("curso")
+    .agg(
+        promedio_nota=("nota", "mean"),
+        total_registros=("nota", "count")
+    )
+    .reset_index()
+)
 
 end = time.perf_counter()
 end_mem = mem_mb()
 
-print("\n[ENFOQUE TRADICIONAL]")
-# print(resultado.head())
+print("\n[ENFOQUE TRADICIONAL - GROUPBY]")
+print(resultado)
+
 print(f"\nFilas: {len(resultado)}")
 print(f"Tiempo: {end - start:.2f} s")
 print(f"Memoria usada: {end_mem - start_mem:.2f} MB")
